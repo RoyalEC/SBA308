f448d7552c271e1c3e8091cd6bf342a11817d4b6
Ernie Crews <141796937+RoyalEC@users.noreply.github.com>
2023-11-30 19:32:45 -0500

    assigment Group

--- sba.js
+++ sba.js
@@ -1,16 +1,34 @@
 //SBA 308 Assignment
 
 const courseInfo {
-    id: number,
-    name: string,
-}
+    id: 101,
+    name: "Introduction to Programming",
+};
 
 const assignmentGroup {
-    id: number,
-    name: string,
-    course_id: number
-    group_weight:
+    id: 201,
+    name: "Programming Assignments",
+    course_id: 101,
+    group_weight: 50,
+    assignments: [{
+        id: 301
+        name: "Assignment 1",
+        due_at: '2023-05-01',
+        points_possible: 100,
+    }{
+        id: 302,
+        name: "Assignment 2",
+        due_at: "2023-05-02",
+        points_possible: 200,
+
+    }{
+        id; 303,
+        name: "Assignment 3",
+        due_at: "2023-05-03",
+        points_possible: 300,
+    }
+    ]
 
 }
 
-const 
+const learnerSubmissions = 
