bd060c17166406ce721acda81a76bbe83c426942
Ernie Crews <141796937+RoyalEC@users.noreply.github.com>
2023-11-30 19:57:28 -0500

    function created

--- sba.js
+++ sba.js
@@ -1,17 +1,17 @@
 //SBA 308 Assignment
 
-const courseInfo {
+const courseInfo = {
     id: 101,
     name: "Introduction to Programming",
 };
 
-const assignmentGroup[ {
+const assignmentGroup = [ {
     id: 201,
     name: "Programming Assignments",
     course_id: 101,
     group_weight: 50,
     assignments: [{
-        id: 301
+        id: 301,
         name: "Assignment 1",
         due_at: '2023-05-01',
         points_possible: 100,
@@ -22,7 +22,7 @@ const assignmentGroup[ {
         points_possible: 200,
 
     },{
-        id; 303,
+        id: 303,
         name: "Assignment 3",
         due_at: "2023-05-03",
         points_possible: 300,
@@ -37,21 +37,36 @@ const learnerSubmissions = [{
     submission:{
         submitted_at: "2023-04-30",
         score: 85,
+    }
     },{
         learner_id: 502,
         assignment_id: 302,
         submission:{
             submitted_at: "2023-4-30",
             score: 90,
-        }, {
+        }},
+         {
         learner_id: 503,
         assignment_id: 303
         submission:{
             submitted_at: "2023-4-30",
             score: 95,
-        }
-        }
-    }
-}]
+        }},
+        
+    }]
 
-function 
+function getLearnerData(courseInfo, assignmentGroup, learnerSubmissions){
+    try { 
+        if (assignmentGroup.course_id !== courseInfo.id){
+            throw new Error("Course ID mismatch.")
+        }
+        const results = [];
+        for (const submission of learnerSubmissions){
+            let assignment = assignmentGroup.assignment.find(a => a.id === submission.assignment.id);
+            if (!assignment) continue;
+        }
+        
+    } catch (error) {
+        
+    }
+}
